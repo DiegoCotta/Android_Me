bf9d865ccf342bc75ede6f8ca75a9f8b85386511
diegocotta <diego.cotta@zup.com.br>
2018-10-11 13:59:13 -0300

    done

--- app/src/main/java/com/example/android/android_me/ui/AndroidMeActivity.java
+++ app/src/main/java/com/example/android/android_me/ui/AndroidMeActivity.java
@@ -21,6 +21,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 import com.example.android.android_me.R;
+import com.example.android.android_me.data.AndroidImageAssets;
 
 // This activity will display a custom Android image composed of three body parts: head, body, and legs
 public class AndroidMeActivity extends AppCompatActivity {
@@ -33,17 +34,31 @@ public class AndroidMeActivity extends AppCompatActivity {
 
         // Create a new head BodyPartFragment
         BodyPartFragment headFragment = new BodyPartFragment();
-
-        // TODO (4) Set the list of image id's for the head fragment and set the position to the second image in the list
+        BodyPartFragment bodyFragment = new BodyPartFragment();
+        BodyPartFragment legsFragment = new BodyPartFragment();
 
         // Add the fragment to its container using a FragmentManager and a Transaction
         FragmentManager fragmentManager = getSupportFragmentManager();
 
+
+        headFragment.setImageIds(AndroidImageAssets.getHeads());
+        headFragment.setListIndex(0);
         fragmentManager.beginTransaction()
                 .add(R.id.head_container, headFragment)
                 .commit();
 
-        // TODO (5) Create and display the body and leg BodyPartFragments
+        bodyFragment.setImageIds(AndroidImageAssets.getBodies());
+        bodyFragment.setListIndex(0);
+        fragmentManager.beginTransaction()
+                .add(R.id.body_container, bodyFragment)
+                .commit();
+
+        legsFragment.setImageIds(AndroidImageAssets.getLegs());
+        legsFragment.setListIndex(0);
+        fragmentManager.beginTransaction()
+                .add(R.id.legs_container, legsFragment)
+                .commit();
+
 
     }
 }
